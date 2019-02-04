3571a64d25705832bb8e977a394a8299815436f3
deda9 <Bassem.Qoulta@gmail.com>
2019-02-04 15:45:04 +0100

    CREATE: Movie Response model + Unit tests

---
+++ Movizy/Network/Response/MoviesResponse.swift
@@ -0,0 +1,12 @@
+
+struct MoviesResponse: Codable {
+    let page, totalResults, totalPages: Int
+    let moviesList: [Movie]
+    
+    enum CodingKeys: String, CodingKey {
+        case page
+        case totalResults = "total_results"
+        case totalPages = "total_pages"
+        case moviesList = "results"
+    }
+}
