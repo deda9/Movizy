f899aa941fd7c7931071046637b1f3a87b09eb02
deda9 <Bassem.Qoulta@gmail.com>
2019-02-04 15:45:32 +0100

    CREATE: Network servers

---
+++ Movizy/Network/Base/Constants.swift
@@ -0,0 +1,17 @@
+struct Constants {
+    struct Pathes {
+        struct Movies {
+            public static let popularList = "/movie/popular"
+            public static let searchMovie = "/search/movie"
+            public static let imageBaseUrl = "https://image.tmdb.org/t/p/w500"
+        }
+    }
+    
+    struct Keys {
+        public static let MovieDB = "9075303a6c0299a1dc0e2d6eaa1cffed"
+    }
+    
+    struct Language {
+        public static let en = "en-US"
+    }
+}
